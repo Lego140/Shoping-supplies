ba4971b9292997377f850b4b2454125fc9718e1b
Gawin <legomonshouwer@gmail.com>
2023-03-01 09:59:12 +0700

    storage 3

--- storageClass.java
+++ storageClass.java
@@ -1,26 +1,48 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class storageClass {
 
     public static void csvScanner() throws IOException{
-        List<String[]> everything = new ArrayList<String[]>();
+        ArrayList<ArrayList<String>> foods = new ArrayList<>();
 
-        FileReader file = new FileReader("Book1.csv");
+        FileReader file = new FileReader("C:\\Users\\woranath\\IdeaProjects\\Shopping list challenge\\src\\Book1.csv");
         Scanner sc = new Scanner(file);
+        //System.out.println(sc.nextLine());
+        int p=0;
 
         while (sc.hasNextLine()){
-            everything.add(new String[]{"","","",""});
+            String values[] = sc.nextLine().split(",");
+            ArrayList<String> row = new ArrayList<String>(Arrays.asList(values));
+            foods.add(row);
+            p++;
         }
 
-
-
-        for (String[] row : everything){
-            Log.i(Tag,row[0]);
+        String [][] foodsArray=new String[p][];
+        for (int i =0; i< p;i++){
+            ArrayList<String> row = foods.get(i);
+            foodsArray[i] = row.toArray(new String[row.size()]);
         }
+
+        for (int y=0; y<p;y++){
+            for (int o=0; o < 3;o++){
+                System.out.println(foodsArray[y][o]);
+            }
+        }
+
+        public static void csvInputer;(){
+
+        }
+
 
     }
+
+
+
+
+
 }
 
 
