cbd50fb7e7a146043f11887558d43bca98b4e9f0
Gawin <legomonshouwer@gmail.com>
2023-03-01 12:42:12 +0700

    storage 4

--- storageClass.java
+++ storageClass.java
@@ -1,8 +1,8 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+
 public class storageClass {
 
     public static void csvScanner() throws IOException{
@@ -27,22 +27,129 @@ public class storageClass {
         }
 
         for (int y=0; y<p;y++){
-            for (int o=0; o < 3;o++){
+            for (int o=0; o < 4;o++){
                 System.out.println(foodsArray[y][o]);
             }
         }
 
-        public static void csvInputer;(){
-
-        }
 
 
     }
 
+    public static void writeCSV(String foodsArray[][]) throws IOException{
+        FileWriter write = new FileWriter("C:\\Users\\woranath\\IdeaProjects\\Shopping list challenge\\src\\Book1.csv");
+
+
+    }
+
+    public static void inputFoods() throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+
+        String categorySTR = "";
+
+
+        System.out.println("| FOOD INPUT |" + "\nWhere are you storing the food?\n1. Freezer\n2. Fridge\n3. Shelf\nEnter 1 or 2 or 3 ");
+
+        while (!sc.hasNextInt()) {
+            System.out.print("Invalid input. Please enter a number 1 2 or 3: ");
+            sc.next();
+        }
+        int category = sc.nextInt();
+
+        while (category < 1 || category > 3) {
+            System.out.print("Invalid input. Please enter 1 2 or 3: ");
+            category = sc.nextInt();
+        }
+
+        switch (category) {
+            case 1:
+                categorySTR = "Freezer";
+
+                break;
+            case 2:
+                categorySTR = "Fridge";
+
+                break;
+            case 3:
+                categorySTR = "Shelf";
+
+                break;
+        }
+
+        System.out.println("| Successful input |\nEnter the name of your item: ");
+
+        while (sc.hasNextInt()) {
+            System.out.print("Invalid input. Please enter name: ");
+            sc.next();
+        }
+        String name = sc.next();
+
+        System.out.println("| Successful input |\nEnter the Exspiary date of the item in YYYY-MM-dd: ");
+        while (!sc.hasNext("\\d\\d\\d\\d-\\d\\d-\\d\\d")) {
+            System.out.print("Invalid input. Please enter Exspiary date of the item in YYYY-MM-dd\nif it is a digit day ex the 4th do 04 instead: ");
+
+            sc.next();
+        }
+        String exp = sc.next();
+        //String currentYear = (Date() + " ").toString().substring(24, 28);
+        int year = Integer.valueOf(exp.substring(0,4));
+        System.out.println(year);
+        //if (year< Integer.valueOf(getDate())){}
+
+        /*
+        while(sc.nextInt()>3||sc.nextInt()<1){
+            System.out.print("!!! PLEASE ENTER VALID INPUT 1 or 2 or 3 !!! : ");
+            sc.next();
+            while (!sc.hasNextInt()){
+
+                System.out.print("!!! PLEASE ENTER VALID INPUT 1 or 2 or 3 !!! : ");
+                sc.next();
+            }
+        }
+
+
+
+        category = sc.nextInt();
+
+        //System.out.println("ur gay"+category);
+
+        switch (category) {
+            case 1:
+                categorySTR = "Freezer";
+                valid = true;
+                break;
+            case 2:
+                categorySTR = "Fridge";
+                valid = true;
+                break;
+            case 3:
+                categorySTR = "Shelf";
+                valid = true;
+                break;
+        }
+        System.out.println(categorySTR);
+
+
+        while (valid == false) {
 
 
 
 
+
+            try {
+                category = sc.nextInt();
+            }catch (InputMismatchException e) {
+                System.out.println("!!! PLEASE ENTER VALID INPUT 1 or 2 or 3 !!!");
+
+            }
+
+
+
+        }
+
+         */
+    }
 }
 
 
